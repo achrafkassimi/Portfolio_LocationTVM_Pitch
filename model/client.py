654b25f8c95de7bb98f7348c252df224f1db1302
achrafkassimi <achraf.kassimi.1995@gmail.com>
2024-09-04 17:28:22 +0100

    change

--- model/client.py
+++ model/client.py
@@ -7,17 +7,17 @@ from sqlalchemy import Column, String
 
 
 
-    # __tablename__ = 'bike'
-    # name = Column(String(128), nullable=False)
-    # code_model = Column(String(128), nullable=False)
-    # Speed_max = Column(String(128), nullable=False)
-    # Puissance = Column(String(128), nullable=False)
-    # detail = Column(String(128), nullable=False)
-    # # image = ??
+# __tablename__ = 'bike'
+# name = Column(String(128), nullable=False)
+# code_model = Column(String(128), nullable=False)
+# Speed_max = Column(String(128), nullable=False)
+# Puissance = Column(String(128), nullable=False)
+# detail = Column(String(128), nullable=False)
+# # image = ??
 
-    # def __init__(self, *args, **kwargs):
-    #     """initializes User"""
-    #     super().__init__(*args, **kwargs)
+# def __init__(self, *args, **kwargs):
+#     """initializes User"""
+#     super().__init__(*args, **kwargs)
 
 
 class Client(Base):
